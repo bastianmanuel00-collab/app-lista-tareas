3d0480f47ddc2f3a39843c6fd4770d2888faf6ca
bastian madariaga <bastianmanuel00@gmail.com>
2026-06-01 09:48:05 -0400

    creacion de campo para escribir

--- mi-lista-tareas/src/App.jsx
+++ mi-lista-tareas/src/App.jsx
@@ -5,8 +5,19 @@ function App() {
   const [tareas, setTareas] = useState ([]);
   const [texto, setTexto]= useState("");
   return (
-    <div className="app">
-      <h1>Mi lista de tareas</h1>
+    <div>
+      <div className="app">
+        <h1>Mi lista de tareas</h1>
+      </div>
+
+      <div className="entrada"> 
+        <input
+          value={texto}
+          onChange={(e) => setTexto(e.target.value)}
+          placeholder="Escribe una tarea"
+        />
+        <button> Agregar</button>
+      </div>
     </div>
   );
 }
