3076bfbed0ad004ad8ae8cb3311b274aab75e1cd
bastian madariaga <bastianmanuel00@gmail.com>
2026-06-01 09:41:30 -0400

    guardar tareas en app.jsx

--- mi-lista-tareas/src/App.jsx
+++ mi-lista-tareas/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [tareas, setTareas] = useState ([]);
+  const [texto, setTexto]= useState("");
   return (
     <div className="app">
       <h1>Mi lista de tareas</h1>
