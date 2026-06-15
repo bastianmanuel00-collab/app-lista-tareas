581ebc730f7b5031aea3511719ef4413485db0aa
Bastian Madariaga <bastianmanuel00@gmail.com>
2026-06-15 02:07:08 -0400

    lista de tareas

--- mi-lista-tareas/src/App.jsx
+++ mi-lista-tareas/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react"; 
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [tareas, setTareas] = useState ([]);
-  const [texto, setTexto]= useState("");
+  const [tareas, setTareas] = useState([]);
+  const [texto, setTexto] = useState("");
+
   function agregarTarea() {
     if (texto.trim() === "") return;
-    const nueva = {
-      id: Date.now(),
-      titulo: texto,
-      hecha: false,
-    };
+    const nueva = { id: Date.now(), titulo: texto, hecha: false };
     setTareas([...tareas, nueva]);
     setTexto("");
-    }
-  return (
-    <div>
-      <div className="app">
-        <h1>Mi lista de tareas</h1>
-      </div>
+  }
 
-      <div className="entrada"> 
+  return (
+    <div className="app">
+      <h1>Mi lista de tareas</h1>
+
+      <div className="entrada">
         <input
           value={texto}
           onChange={(e) => setTexto(e.target.value)}
           placeholder="Escribe una tarea"
         />
-        <button> Agregar</button>
+        <button onClick={agregarTarea}>Agregar</button>
       </div>
+
+      <ul>
+        {tareas.map((t) => (
+          <li key={t.id}>
+            <span>{t.titulo}</span>
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
